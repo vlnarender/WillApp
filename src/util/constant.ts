06ff216027acc2a31cbe4f29731665067ae60479
suraj kumar <surajknkumar@gmail.com>
2020-11-04 19:19:56 +0530

    Adding New API

--- src/util/constant.ts
+++ src/util/constant.ts
@@ -4,16 +4,16 @@ type ENV_TYPE = 'PROD' | 'DEV' | 'TEST';
 
 const CONFIGS: Record<ENV_TYPE, {url: string}> = {
   PROD: {
-    url: 'http://15.206.88.76/will_backend/public/api',
+    url: 'http://65.0.41.161/will_backend/public/api/',
   },
   DEV: {
     url:
       Platform.OS === 'ios'
-        ? 'http://15.207.112.22/will_backend/public/api'
-        : 'http://15.207.112.22/will_backend/public/api',
+        ? 'http://65.0.41.161/will_backend/public/api/'
+        : 'http://65.0.41.161/will_backend/public/api/',
   },
   TEST: {
-    url: 'http://15.206.88.76/will_backend/public/api',
+    url: 'http://65.0.41.161/will_backend/public/api/',
   },
 };
 
